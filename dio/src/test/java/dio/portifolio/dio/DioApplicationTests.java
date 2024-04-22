86b2e7de1ab7cde7dd60ff129da0d1e96c0380d9
matheusP <matheuspgvc.mp@gmail.com>
2024-04-22 20:56:02 -0300

    first commit

---
+++ dio/src/test/java/dio/portifolio/dio/DioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.portifolio.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
